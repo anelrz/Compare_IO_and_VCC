0124f06cfe509db70501d9ea1650dadc6b7f61b7
anelrz <skobylarz@gmail.com>
2015-09-23 17:54:30 +0200

    .

--- Compare_IO_and_VCC/Form1.Designer.cs
+++ Compare_IO_and_VCC/Form1.Designer.cs
@@ -32,34 +32,13 @@
             this.button1 = new System.Windows.Forms.Button();
             this.Load_VCC = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
             this.button2 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.dataSet1 = new System.Data.DataSet();
-            this.dataTable1 = new System.Data.DataTable();
-            this.dataColumn1 = new System.Data.DataColumn();
-            this.dataColumn2 = new System.Data.DataColumn();
-            this.dataColumn3 = new System.Data.DataColumn();
-            this.dataColumn4 = new System.Data.DataColumn();
-            this.dataColumn5 = new System.Data.DataColumn();
-            this.dataColumn6 = new System.Data.DataColumn();
-            this.dataColumn7 = new System.Data.DataColumn();
-            this.dataColumn8 = new System.Data.DataColumn();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
-            this.iDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.tagNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.rangeLowDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.rangeHighDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.alertHighDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dangerHighDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.alertLowDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dangerLowDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.tableLayoutPanel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataTable1)).BeginInit();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -67,33 +46,32 @@
             this.tableLayoutPanel1.ColumnCount = 2;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 200F));
-            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 6);
-            this.tableLayoutPanel1.Controls.Add(this.Load_VCC, 1, 5);
-            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 6);
-            this.tableLayoutPanel1.Controls.Add(this.button2, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.Load_VCC, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.treeView1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 1, 3);
             this.tableLayoutPanel1.Controls.Add(this.listBox1, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 7;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 26.86567F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 73.13433F));
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 66.66666F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 52F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(962, 542);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(450, 542);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // button1
             // 
             this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.button1.Enabled = false;
-            this.button1.Location = new System.Drawing.Point(765, 518);
+            this.button1.Location = new System.Drawing.Point(253, 513);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(194, 21);
+            this.button1.Size = new System.Drawing.Size(194, 26);
             this.button1.TabIndex = 0;
             this.button1.Text = "Compare";
             this.button1.UseVisualStyleBackColor = true;
@@ -102,9 +80,9 @@
             // Load_VCC
             // 
             this.Load_VCC.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.Load_VCC.Location = new System.Drawing.Point(765, 492);
+            this.Load_VCC.Location = new System.Drawing.Point(253, 484);
             this.Load_VCC.Name = "Load_VCC";
-            this.Load_VCC.Size = new System.Drawing.Size(194, 20);
+            this.Load_VCC.Size = new System.Drawing.Size(194, 23);
             this.Load_VCC.TabIndex = 1;
             this.Load_VCC.Text = "Load VCC files";
             this.Load_VCC.UseVisualStyleBackColor = true;
@@ -114,17 +92,27 @@
             // 
             this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.textBox1.Enabled = false;
-            this.textBox1.Location = new System.Drawing.Point(3, 518);
+            this.textBox1.Location = new System.Drawing.Point(3, 513);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(756, 20);
+            this.textBox1.Size = new System.Drawing.Size(244, 20);
             this.textBox1.TabIndex = 3;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(3, 3);
+            this.treeView1.Name = "treeView1";
+            this.tableLayoutPanel1.SetRowSpan(this.treeView1, 5);
+            this.treeView1.Size = new System.Drawing.Size(244, 504);
+            this.treeView1.TabIndex = 4;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
             // 
             // button2
             // 
             this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button2.Location = new System.Drawing.Point(765, 466);
+            this.button2.Location = new System.Drawing.Point(253, 455);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(194, 20);
+            this.button2.Size = new System.Drawing.Size(194, 23);
             this.button2.TabIndex = 5;
             this.button2.Text = "Load Excel IO lists";
             this.button2.UseVisualStyleBackColor = true;
@@ -135,100 +123,11 @@
             this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.listBox1.FormattingEnabled = true;
             this.listBox1.IntegralHeight = false;
-            this.listBox1.Location = new System.Drawing.Point(765, 3);
+            this.listBox1.Location = new System.Drawing.Point(253, 3);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(194, 99);
+            this.listBox1.Size = new System.Drawing.Size(194, 261);
             this.listBox1.TabIndex = 6;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.AutoGenerateColumns = false;
-            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedHeaders;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.iDDataGridViewTextBoxColumn,
-            this.tagNameDataGridViewTextBoxColumn,
-            this.rangeLowDataGridViewTextBoxColumn,
-            this.rangeHighDataGridViewTextBoxColumn,
-            this.alertHighDataGridViewTextBoxColumn,
-            this.dangerHighDataGridViewTextBoxColumn,
-            this.alertLowDataGridViewTextBoxColumn,
-            this.dangerLowDataGridViewTextBoxColumn});
-            this.dataGridView1.DataMember = "Tags";
-            this.dataGridView1.DataSource = this.dataSet1;
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
-            this.dataGridView1.Name = "dataGridView1";
-            this.tableLayoutPanel1.SetRowSpan(this.dataGridView1, 6);
-            this.dataGridView1.Size = new System.Drawing.Size(756, 509);
-            this.dataGridView1.TabIndex = 7;
-            // 
-            // dataSet1
-            // 
-            this.dataSet1.DataSetName = "TagsDataSet";
-            this.dataSet1.Tables.AddRange(new System.Data.DataTable[] {
-            this.dataTable1});
-            // 
-            // dataTable1
-            // 
-            this.dataTable1.Columns.AddRange(new System.Data.DataColumn[] {
-            this.dataColumn1,
-            this.dataColumn2,
-            this.dataColumn3,
-            this.dataColumn4,
-            this.dataColumn5,
-            this.dataColumn6,
-            this.dataColumn7,
-            this.dataColumn8});
-            this.dataTable1.Constraints.AddRange(new System.Data.Constraint[] {
-            new System.Data.UniqueConstraint("Constraint1", new string[] {
-                        "ID"}, true)});
-            this.dataTable1.PrimaryKey = new System.Data.DataColumn[] {
-        this.dataColumn1};
-            this.dataTable1.TableName = "Tags";
-            // 
-            // dataColumn1
-            // 
-            this.dataColumn1.AllowDBNull = false;
-            this.dataColumn1.AutoIncrement = true;
-            this.dataColumn1.ColumnName = "ID";
-            this.dataColumn1.DataType = typeof(short);
-            // 
-            // dataColumn2
-            // 
-            this.dataColumn2.ColumnName = "TagName";
-            // 
-            // dataColumn3
-            // 
-            this.dataColumn3.ColumnName = "RangeLow";
-            this.dataColumn3.DataType = typeof(double);
-            // 
-            // dataColumn4
-            // 
-            this.dataColumn4.ColumnName = "RangeHigh";
-            this.dataColumn4.DataType = typeof(double);
-            // 
-            // dataColumn5
-            // 
-            this.dataColumn5.ColumnName = "AlertHigh";
-            this.dataColumn5.DataType = typeof(double);
-            // 
-            // dataColumn6
-            // 
-            this.dataColumn6.ColumnName = "DangerHigh";
-            this.dataColumn6.DataType = typeof(double);
-            // 
-            // dataColumn7
-            // 
-            this.dataColumn7.ColumnName = "AlertLow";
-            this.dataColumn7.DataType = typeof(double);
-            // 
-            // dataColumn8
-            // 
-            this.dataColumn8.ColumnName = "DangerLow";
-            this.dataColumn8.DataType = typeof(double);
             // 
             // openFileDialog1
             // 
@@ -244,64 +143,11 @@
             this.openFileDialog2.FileName = "openFileDialog2";
             this.openFileDialog2.Multiselect = true;
             // 
-            // iDDataGridViewTextBoxColumn
-            // 
-            this.iDDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
-            this.iDDataGridViewTextBoxColumn.DataPropertyName = "ID";
-            this.iDDataGridViewTextBoxColumn.HeaderText = "ID";
-            this.iDDataGridViewTextBoxColumn.Name = "iDDataGridViewTextBoxColumn";
-            this.iDDataGridViewTextBoxColumn.ReadOnly = true;
-            this.iDDataGridViewTextBoxColumn.Width = 43;
-            // 
-            // tagNameDataGridViewTextBoxColumn
-            // 
-            this.tagNameDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
-            this.tagNameDataGridViewTextBoxColumn.DataPropertyName = "TagName";
-            this.tagNameDataGridViewTextBoxColumn.HeaderText = "TagName";
-            this.tagNameDataGridViewTextBoxColumn.Name = "tagNameDataGridViewTextBoxColumn";
-            this.tagNameDataGridViewTextBoxColumn.Width = 79;
-            // 
-            // rangeLowDataGridViewTextBoxColumn
-            // 
-            this.rangeLowDataGridViewTextBoxColumn.DataPropertyName = "RangeLow";
-            this.rangeLowDataGridViewTextBoxColumn.HeaderText = "RangeLow";
-            this.rangeLowDataGridViewTextBoxColumn.Name = "rangeLowDataGridViewTextBoxColumn";
-            // 
-            // rangeHighDataGridViewTextBoxColumn
-            // 
-            this.rangeHighDataGridViewTextBoxColumn.DataPropertyName = "RangeHigh";
-            this.rangeHighDataGridViewTextBoxColumn.HeaderText = "RangeHigh";
-            this.rangeHighDataGridViewTextBoxColumn.Name = "rangeHighDataGridViewTextBoxColumn";
-            // 
-            // alertHighDataGridViewTextBoxColumn
-            // 
-            this.alertHighDataGridViewTextBoxColumn.DataPropertyName = "AlertHigh";
-            this.alertHighDataGridViewTextBoxColumn.HeaderText = "AlertHigh";
-            this.alertHighDataGridViewTextBoxColumn.Name = "alertHighDataGridViewTextBoxColumn";
-            // 
-            // dangerHighDataGridViewTextBoxColumn
-            // 
-            this.dangerHighDataGridViewTextBoxColumn.DataPropertyName = "DangerHigh";
-            this.dangerHighDataGridViewTextBoxColumn.HeaderText = "DangerHigh";
-            this.dangerHighDataGridViewTextBoxColumn.Name = "dangerHighDataGridViewTextBoxColumn";
-            // 
-            // alertLowDataGridViewTextBoxColumn
-            // 
-            this.alertLowDataGridViewTextBoxColumn.DataPropertyName = "AlertLow";
-            this.alertLowDataGridViewTextBoxColumn.HeaderText = "AlertLow";
-            this.alertLowDataGridViewTextBoxColumn.Name = "alertLowDataGridViewTextBoxColumn";
-            // 
-            // dangerLowDataGridViewTextBoxColumn
-            // 
-            this.dangerLowDataGridViewTextBoxColumn.DataPropertyName = "DangerLow";
-            this.dangerLowDataGridViewTextBoxColumn.HeaderText = "DangerLow";
-            this.dangerLowDataGridViewTextBoxColumn.Name = "dangerLowDataGridViewTextBoxColumn";
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(962, 542);
+            this.ClientSize = new System.Drawing.Size(450, 542);
             this.Controls.Add(this.tableLayoutPanel1);
             this.MinimumSize = new System.Drawing.Size(380, 270);
             this.Name = "Form1";
@@ -309,9 +155,6 @@
             this.Text = "Form1";
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataTable1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -323,29 +166,11 @@
         private System.Windows.Forms.Button Load_VCC;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TreeView treeView1;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.OpenFileDialog openFileDialog2;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Data.DataSet dataSet1;
-        private System.Data.DataTable dataTable1;
-        private System.Data.DataColumn dataColumn1;
-        private System.Data.DataColumn dataColumn2;
-        private System.Data.DataColumn dataColumn3;
-        private System.Data.DataColumn dataColumn4;
-        private System.Data.DataColumn dataColumn5;
-        private System.Data.DataColumn dataColumn6;
-        private System.Data.DataColumn dataColumn7;
-        private System.Data.DataColumn dataColumn8;
-        private System.Windows.Forms.DataGridViewTextBoxColumn iDDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn tagNameDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn rangeLowDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn rangeHighDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn alertHighDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dangerHighDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn alertLowDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dangerLowDataGridViewTextBoxColumn;
     }
 }
 
